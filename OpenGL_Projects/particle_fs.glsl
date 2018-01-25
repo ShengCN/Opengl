db37b7f0eba120b27b1c209e93646d2252444587
ShengCN <sheng@keylol.com>
2018-01-25 14:36:17 -0500

    Add plot.

--- OpenGL_Projects/particle_fs.glsl
+++ OpenGL_Projects/particle_fs.glsl
@@ -9,8 +9,13 @@ out vec4 fragcolor;
 in vec2 tex_coord;
 in vec4 gl_FragCoord;
 
-void main(void)
-{   
+float plot(vec2 st,float pct)
+{
+    return smoothstep(pct-0.02,pct,st.y) - smoothstep(pct,pct+0.02,st.y);
+}
+
+void Draw_Bubble()
+{
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     vec2 mouseUV = mouse.xy / resolution.xy;
     mouseUV.y = 1.0 - mouseUV.y;
@@ -28,6 +33,25 @@ void main(void)
     }
     else
         fragcolor = vec4(uv.x,uv.y,abs(sin(time)),1.0);
+}
+
+void Draw_Plot()
+{
+    vec2 uv = gl_FragCoord.xy / resolution.xy;
+    vec2 mouseUV = mouse.xy / resolution.xy;
+    mouseUV.y = 1.0 - mouseUV.y;
+
+    float y = pow(uv.x,2);
+    vec3 color = vec3(y);
+    float pct = plot(uv,y);
+    color = (1.0-pct)*color + pct * vec3(0.0,1.0,0.0);
+    fragcolor = vec4(color,1.0);
+}
+
+void main(void)
+{   
+    Draw_Plot();
+    //Draw_Bubble();
 }
 
 
