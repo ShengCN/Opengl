1bd50b80251df92704e297314d8a74e509fec2cd
ShengCN <sheng@keylol.com>
2018-04-14 06:02:11 -0400

    Raymarching template.

--- OpenGL_Projects/shadertoy_fs.glsl
+++ OpenGL_Projects/shadertoy_fs.glsl
@@ -10,229 +10,73 @@ uniform float slider;
 
 out vec4 fragColor;
 
-#define MAX_STEPS 64
-#define EPSILON .001
-
-#define M_PI 3.14159265358979
-
-const vec3 LightSource = vec3(2., 1.5, 0);
-
-vec3 WaterColor = vec3(0.4, 0.9, 1);
-
-const float WaterHeight = 0.0;
-const float MaxWaveAmplitude = 0.04;
-
-const float HeightPool = 1.;
-const float HalfSizePool = 3.;
-const float DepthPool = 2.;
-
-struct MaterialInfo {
-	vec3 Kd;
-	float Shininess;
-};
-
-float CyclicTime()
+float sdSphere(vec3 p)
 {
-	return mod(iTime, 30.);
+	return distance(p,vec3(0.0)) - 0.5;
 }
 
-float WaveAmplitude() {
-	return MaxWaveAmplitude * exp(-CyclicTime() / 10.);
-}
-
-float WaterWave(vec3 a) {
-	return WaveAmplitude() * sin((2. * a.x * a.x + 2. * a.z * a.z) - 10. * CyclicTime());
-}
-
-float BallOscillation() {
-	return sin(5. * CyclicTime() + 4.) * exp(-CyclicTime() / 6.) + 0.3;
-}
-
-float PoolBottom(vec3 a) {
-	return a.y + DepthPool + .01;
-}
-
-float BackWall(vec3 a) {
-	return a.z + HalfSizePool + .01;
-}
-
-float LeftWall(vec3 a) {
-	return a.x + HalfSizePool + .01;
-}
-
-float WaterSurface(vec3 a) {
-	vec3 sz = vec3(HalfSizePool, 0, HalfSizePool);
-	return length(max(abs(a + vec3(0, WaterWave(a), 0)) - sz, 0.));
-}
-
-float Pool(vec3 a) {
-	return min(PoolBottom(a), min(LeftWall(a), BackWall(a)));
-}
-
-float Ball(vec3 a) {
-	return length(a + vec3(0., BallOscillation(), 0.)) - 0.75;
-}
-
-float Scene(vec3 a) {
-	return min(WaterSurface(a), min(Ball(a), Pool(a)));
-}
-
-bool IsWaterSurface(vec3 a)
+/**********************
+	0	background
+	1	sphere
+**********************/
+int GetSurfaceID(vec3 pos)
 {
-	float closest = Ball(a);
-	float dist = Pool(a);
-	if (dist < closest) {
-		closest = dist;
-	}	
-	dist = WaterSurface(a);
-	if (dist < closest) {
-		return true;
-	}
-	return false;
+	float dis = sdSphere(pos);
+	
+	if(dis<0.001)
+		return 1;
+	else
+		return 0;
 }
 
-bool IsWater(vec3 pos)
+float map(vec3 pos)
 {
-	return (pos.y < (WaterHeight - MaxWaveAmplitude));
+	return sdSphere(pos);
 }
 
-vec3 PoolColor(vec3 pos) {		
-	if ((pos.y > HeightPool) || (pos.x > HalfSizePool) || (pos.z > HalfSizePool)) 
+vec3 Shade(vec3 ro, vec3 rd, float t)
+{
+	int id = GetSurfaceID(ro+rd*t);
+
+	if(id == 1)
+		return vec3(0.5);
+	if(id == 0)
 		return vec3(0.0);
-	float tileSize = 0.2;
-	float thickness = 0.015;
-	vec3 thick = mod(pos, tileSize);
-	if ((thick.x > 0.) && (thick.x < thickness) || (thick.y > 0.) && (thick.y < thickness) || (thick.z > 0.) && (thick.z < thickness))
-		return vec3(1);
-	return vec3(sin(floor((pos.x + 1.) / tileSize)) * cos(floor((pos.y + 1.) / tileSize)) * sin(floor((pos.z + 1.) / tileSize)) + 3.);
 }
 
-MaterialInfo Material(vec3 a) {
-	MaterialInfo m = MaterialInfo(vec3(.5, .56, 1.), 50.);
-	float closest = Ball(a);
-
-	float dist = WaterSurface(a);
-	if (dist < closest) {
-		closest = dist;
-		m.Kd = WaterColor;
-		m.Shininess = 120.;
-	}
-	dist = Pool(a);
-	if (dist < closest) {
-		m.Kd = PoolColor(a);		
-		m.Shininess = 0.;
-	}
-	return m;
-}
-
-vec3 Normal(vec3 a) {
-	vec2 e = vec2(.001, 0.);
-	float s = Scene(a);
-	return normalize(vec3(
-		Scene(a+e.xyy) - s,
-		Scene(a+e.yxy) - s,
-		Scene(a+e.yyx) - s));
-}
-
-float Occlusion(vec3 at, vec3 normal) {
-	float b = 0.;
-	for (int i = 1; i <= 4; ++i) {
-		float L = .06 * float(i);
-		float d = Scene(at + normal * L);		
-		b += max(0., L - d);
-	}
-	return min(b, 1.);
-}
-
-vec3 LookAt(vec3 pos, vec3 at, vec3 rDir) {
-	vec3 f = normalize(at - pos);
-	vec3 r = cross(f, vec3(0., 1., 0.));
-	vec3 u = cross(r, f);
-	return mat3(r, u, -f) * rDir;
-}
-
-float Trace(vec3 rPos, vec3 rDir, float distMin) {
-	float L = distMin;
-	for (int i = 0; i < MAX_STEPS; ++i) {
-		float d = Scene(rPos + rDir * L);
-		L += d;
-		if (d < EPSILON * L) break;
-	}
-	return L;
-}
-
-vec3 Lighting(vec3 at, vec3 normal, vec3 eye, MaterialInfo m, vec3 lColor, vec3 lPos) {
-	vec3 lDir = lPos - at;
-	
-	vec3 lDirN = normalize(lDir);
-	float t = Trace(at, lDirN, EPSILON*2.);
-	if (t < length(lDir)) {
-		vec3 pos = at + lDirN * t;
-		if(!IsWaterSurface(pos))
-			return vec3(0.);
-	}
-	vec3 color = m.Kd * lColor * max(0., dot(normal, normalize(lDir)));
-	
-	if (m.Shininess > 0.) {
-		vec3 h = normalize(normalize(lDir) + normalize(eye - at));
-		color += lColor * pow(max(0., dot(normal, h)), m.Shininess) * (m.Shininess + 8.) / 25.;
-	}
-	return color / dot(lDir, lDir);
-}
-
-vec3 Shade(vec3 rpos, vec3 rdir, float t)
+float Intersect(vec3 ro, vec3 rd)
 {
-	vec3 pos = rpos + rdir * t;
-	vec3 nor = Normal(pos);
-	
-	bool waterSurface = IsWaterSurface(pos);
-	bool water = IsWater(pos);
-	vec3 waterSurfaceLight = vec3(0);
-	if (waterSurface)
+	float t = 0.0;
+	for(int i =0; i < 128; ++i)
 	{
-		vec3 refractionDir = refract(normalize(rdir), nor, 0.9);                                // refraction direction
-
-		waterSurfaceLight = Lighting(pos, nor, rpos, Material(pos), vec3(1.), LightSource);     // water surface color
-
-		float wt = Trace(pos, refractionDir, 0.03);		                                        // trace from refraction direction
-		pos += refractionDir * wt;                                                              // new hit position, only consider one time refraction
-		nor = Normal(pos);                                                                      // new normal vector for calculate lightning
+		float dis = map(ro + t * rd);	
+		if(dis < 0.001)	return t;
+		t += dis;
 	}
-	MaterialInfo mat = Material(pos);                                                           // new place's material
 
-	vec3 color = .11 * (1. - Occlusion(pos, nor)) * mat.Kd;                                     // new place color with AO
-
-	color += Lighting(pos, nor, rpos, mat, vec3(1.), LightSource);                              // new place lighting result
-	
-	if (water || waterSurface) {
-		color *= WaterColor;                                                                    // filter by water color
-		if (waterSurface)                                                                       // consider refaction result
-			color += waterSurfaceLight;
-	}
-	return color;
+	return 	t;
 }
 
-vec3 Camera(vec2 px) {
-	vec2 uv = px.xy / iResolution.xy * 2. - 1.;	
-	uv.x *= iResolution.x / iResolution.y;
-	vec3 rayStart = vec3(3.5, 1.7, 6.);
-	vec3 rayDirection = LookAt(rayStart, vec3(0, -1, 0), normalize(vec3(uv, -2.)));
+vec3 Camera()
+{
+	vec2 p = (-iResolution.xy + 2.0*gl_FragCoord.xy) / iResolution.y;
 	
-	float path = Trace(rayStart, rayDirection, 0.);	
-	return Shade(rayStart, rayDirection, path);
+	// camera anim
+    vec3 ro = 3.0*vec3( 0.0, 0.0, 3.0);
+	vec3 ta = vec3(0.0);
+
+    // camera matrix	
+	vec3  cw = normalize( ta-ro );							 	// forward ray
+	vec3  cu = normalize( cross(cw,vec3(0.0,1.0,0.0)) );		// x
+	vec3  cv = normalize( cross(cu,cw) );						// y
+	vec3  rd = normalize( p.x*cu + p.y*cv + 1.7*cw );
+
+	float t = Intersect(ro,rd);
+	return Shade(ro,rd,t);
 }
 
 void main()
 {
-    vec2 uv = gl_FragCoord.xy / iResolution.xy * 2. - 1.;	
-	uv.x *= iResolution.x / iResolution.y;
-    float radius = sqrt(3.5*3.5 + 6.0 * 6.0);
-	vec3 rayStart = 1.2*vec3(radius*sin(abs(fract(iTime*0.1)-0.5)*3.5), 1.7, radius*cos(abs(fract(iTime*0.1)-0.5)*3.5));
-	vec3 rayDirection = LookAt(rayStart, vec3(0, -1, 0), normalize(vec3(uv, -2.)));
-	
-	float path = Trace(rayStart, rayDirection, 0.);	
-
-    vec3 col = Shade(rayStart, rayDirection, path);
-
-	fragColor = vec4(col, 1.);
+	vec3 col = Camera();	
+    fragColor=vec4(col,1.0);
 }
